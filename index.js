5ff2d0500a4b1a651d40b669f0163e61deba6884
raju <rajubobbili345@gmail.com>
2023-04-10 12:51:32 +0000

    introduction

---
+++ index.js
@@ -0,0 +1,8 @@
+const className = "greeting";
+const element = (
+  <div>
+    <h1 className={className}>Hello Raju!</h1>
+    <p>Good see you</p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
